efea22fb2c7c13c69668a2092321e977b9f7b5d0
VladiSLAV <svladislav876@gmail.com>
2022-12-11 21:43:34 +0300

    metadata package extracters introduced

---
+++ Reader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCLL
+{
+    public abstract class MetadataReader<ExpectedOutput>
+    {
+        public ExpectedOutput Read(MetadataPackage input)
+        {
+
+        }
+    }
+}
